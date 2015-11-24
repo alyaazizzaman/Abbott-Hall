5b20c351f964dc3a2a5dfa8a0b96de7d397c49ff
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2015-11-24 14:47:52 -0700

    built main hall page and added music

---
+++ app3.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', 'rpgcavespooky.ogg');
+  audioElement.setAttribute('autoplay', 'autoplay');
+  audioElement.play();
+});
