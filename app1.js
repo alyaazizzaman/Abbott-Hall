1d892e29203c50ad740bd2f2c2b047786a930a49
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2015-11-24 11:15:22 -0700

    added .fadeIn() jquery animation to Adrian's Letter, changed file names

---
+++ app1.js
@@ -0,0 +1,38 @@
+$("body").click(function() {
+  $(".letter").hide().fadeIn("slow");
+  console.log('hello');
+});
+
+// set html p's to hidden
+
+// $(".letter").click(function() {
+//   $("#p1").hide().fadeIn("slow");
+// });
+
+// $(".letter").click(function() {
+//   $("#p2").hide().fadeIn("slow");
+// });
+
+// $(".letter").click(function() {
+//   $("#p3").hide().fadeIn("slow");
+// });
+
+// $(".letter").click(function() {
+//   $("#p4").hide().fadeIn("slow");
+// });
+
+// $(".letter").click(function() {
+//   $("#p5").hide().fadeIn("slow");
+// });
+
+// $(".letter").click(function() {
+//   $("#p6").hide().fadeIn("slow");
+// });
+
+// $(".letter").click(function() {
+//   $("#p7").hide().fadeIn("slow");
+// });
+
+// $(".letter").click(function() {
+//   $("#p8").hide().fadeIn("slow");
+// });
