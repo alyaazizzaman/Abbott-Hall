1479cbb61e68f6adc7bc3f7323ca367d971c1142
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2015-11-24 22:34:18 -0700

    added timing to text in alternate worlds

--- app10.js
+++ app10.js
@@ -4,3 +4,27 @@ $(document).ready(function() {
   audioElement.setAttribute('autoplay', 'autoplay');
   audioElement.play();
 });
+
+$(document).ready(function() {
+  $("p").hide();
+});
+
+$(document).ready(function() {
+  $(".one").hide().fadeIn(6000, function() {
+    $(".two").fadeIn(6000, function() {
+      $(".three").fadeIn(6000, function () {
+        $(".four").fadeIn(3000, function () {
+          $(".five").fadeIn(3000, function () {
+            $(".six").fadeIn(3000, function () {
+              $(".seven").fadeIn(3000, function () {
+                $(".eight").fadeIn(3000, function () {
+                  $(".nine").fadeIn(3000);
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
--- app9.js
+++ app9.js
@@ -4,3 +4,23 @@ $(document).ready(function() {
   audioElement.setAttribute('autoplay', 'autoplay');
   audioElement.play();
 });
+
+$(document).ready(function() {
+  $("p").hide();
+});
+
+$(document).ready(function() {
+  $(".one").hide().fadeIn(6000, function() {
+    $(".two").fadeIn(6000, function() {
+      $(".three").fadeIn(4000, function () {
+        $(".four").fadeIn(3000, function () {
+          $(".five").fadeIn(3000, function () {
+            $(".six").fadeIn(3000, function () {
+              $(".seven").fadeIn(3000);
+            });
+          });
+        });
+      });
+    });
+  });
+});
